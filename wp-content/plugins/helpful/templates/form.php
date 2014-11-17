b5c78d128a8d47d30f60d1c768f4ed23658d713c
Giel Berkers <info@gielberkers.com>
2014-11-17 20:16:26 +0100

    Update form.php

--- wp-content/plugins/helpful/templates/form.php
+++ wp-content/plugins/helpful/templates/form.php
@@ -1,12 +1,12 @@
 <?php if(!Helpful::hasVisitorRated(get_the_ID())): ?>
 <form action="<?php echo Helpful::getFormUrl(); ?>" method="post" id="helpful-rating">
-	<div>
+    <div>
         <h3>Was this post helpful for you?</h3>
     </div>
     <label class="rating-1"><input type="radio" name="rating" value="1">Useless!</label>
     <label class="rating-3"><input type="radio" name="rating" value="3">It's okay i guess...</label>
     <label class="rating-5"><input type="radio" name="rating" value="5">It was awesome!</label>
-	<div class="why">
+    <div class="why">
         <label for="why">Why?</label>
         <textarea name="why" id="why" cols="30" rows="10" placeholder="Could you provide me some feedback?"></textarea>
     </div>
